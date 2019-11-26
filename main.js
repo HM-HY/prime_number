2e3dc921228a957859887c3f84b5d326916e2c65
HM-HY <57683905+HM-HY@users.noreply.github.com>
2019-11-26 13:22:50 +0400

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,17 @@
 const util = require("./myUtils.js");
-
-console.log(util.isPrime(2));
-
+const reader = require('readline-sync');
+const num1 = reader.questionInt("Please insert a positive number = ");
+const num2 = reader.questionInt("Please insert another positive number = ");
+let arrPrime = [];
+let a = num1;
+let b = num2;
+if(num1>num2) {
+    let x = a;
+    a = b;
+    b = x;
+    }
+for(let x = a; x<=b; x++) {
+    if(util.isPrime(x))
+        arrPrime.push(x);
+    }
+util.arrPrint(arrPrime);
